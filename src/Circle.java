8c010ca3439159c96f7d0fa4166a2fe21146c7ff
Abz21 <70847776+Abz21@users.noreply.github.com>
2022-01-19 11:44:17 +0530

    Initial Commit

---
+++ src/Circle.java
@@ -0,0 +1,9 @@
+public class Circle
+{
+    public static void main(String args[])
+    {
+        double r= 7.55;
+        double  area=(22*r*r)/7 ;
+        System.out.println("Area of Circle is: " + area);
+    }
+}
---
+++ src/CircleTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArmstrongTest {
+    @Test
+    public void check() {
+        Circle checker = new Circle();
+        assertEquals(7.55,7.55);
+    }
+}
