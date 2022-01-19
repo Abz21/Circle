4f64f9c07b7be9c7e32e4c220d98f2fdc0c09849
Abz21 <70847776+Abz21@users.noreply.github.com>
2022-01-19 17:47:52 +0530

    HelloWorld Commit

--- src/Circle.java
+++ src/Circle.java
@@ -1,9 +1,10 @@
 public class Circle
 {
-    public static void main(String args[])
+    public static void main(String[] args)
     {
         double r= 7.55;
         double  area=(22*r*r)/7 ;
+
         System.out.println("Area of Circle is: " + area);
     }
 }
